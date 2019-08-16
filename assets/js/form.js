ad35a6cadde0d800e29f4bfc9488e26f73914ed2
unknown <samvthom16@gmail.com>
2019-08-16 12:21:06 +0530

    Move code to orbit-bundle

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,47 +1,6 @@
 jQuery(document).ready(function(){
 
-  jQuery( '[data-behaviour~="orbit-nested-dropdown"]' ).each( function(){
-
-    var $el             = jQuery( this ),
-      $cats_dropdown    = $el.find( '.cats select' ),
-      $subcats_dropdown = $el.find( '.subcats select' ),
-      $cloneSubDropdown = $el.find( '.subcats select' ).clone();  // Clones all subcats from dropdown
-
-    function updateSubDropdown(){
-
-      var currentCategoryValue = $cats_dropdown.val();
-
-      $subcats_dropdown.find( 'option' ).remove();
-
-      var $options;
-
-      if( currentCategoryValue > 0 ){
-        $options = $cloneSubDropdown.find( 'option[data-parent~="' + currentCategoryValue + '"]' ).clone();
-
-        var $defaultOption = jQuery( document.createElement( 'option' ) );
-        $defaultOption.val( 0 );
-        $defaultOption.html('Select');
-        $defaultOption.appendTo( $subcats_dropdown );
-      }
-      else{
-        $options = $cloneSubDropdown.find('option').clone();
-        $options.first().val(0);
-      }
-
-      $options.appendTo( $subcats_dropdown );
-
-      $subcats_dropdown.val(0);
-
-    }
-
-    // change subservices when the main service is changed
-    $cats_dropdown.change( function( ev ){
-
-      updateSubDropdown();
-
-    });
-
-  });
+  
 
 
 
--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'VARTA_VERSION', '1.0.5' );
+define( 'VARTA_VERSION', '1.0.6' );
 
 add_action('wp_enqueue_scripts',function(){
   wp_enqueue_style('varta-style', get_stylesheet_directory_uri().'/assets/css/varta.css', array('sp-core-style'), VARTA_VERSION );
@@ -18,63 +18,7 @@ add_filter( 'orbit-nested-dropdown-label', function( $label, $atts ){
 
 }, 2, 10 );
 
-function nested_dropdown( $atts ){
 
-  $terms = get_terms( array(
-    'taxonomy'    => $atts['typeval'],
-    'hide_empty'  => false,
-    'orderby'     => 'term_id'
-  ) );
-
-  $cats = array();
-  $subcats = array();
-  foreach ( $terms as $term ) {
-    if( $term->parent ){
-      array_push( $subcats, array(
-        'name'    => $term->name,
-        'slug'    => $term->term_id,
-        'parent'  => $term->parent
-      ) );
-    }
-    else{
-      array_push( $cats, array(
-        'name'    => $term->name,
-        'slug'    => $term->term_id,
-        'parent'  => $term->parent
-      ) );
-    }
-  }
-
-  $orbit_form_field = ORBIT_FORM_FIELD::getInstance();
-
-  $param = "tax_" . $atts['typeval'];
-  $name_param = $param . "[]";
-  $values = $_GET[ $param ];
-
-  _e( "<div data-behaviour='orbit-nested-dropdown'>" );
-
-  _e( "<div class='cats'>" );
-  $orbit_form_field->display( array(
-    'label'   => $atts['label'],
-    'type'    => 'dropdown',
-    'name'    => $name_param,
-    'items'   => $cats,
-    'value'   => is_array( $values ) ? $values[0] : ""
-  ) );
-  _e( "</div>" );
-
-  _e( "<div class='subcats'>" );
-  $orbit_form_field->display( array(
-    'label'           => apply_filters( 'orbit-nested-dropdown-label', 'Select Sub', $atts ),
-    'type'            => 'dropdown',
-    'name'            => $name_param,
-    'items'           => $subcats,
-    'value'           => ( is_array( $values ) && count( $values ) > 1 ) ? $values[1] : ""
-  ) );
-  _e( "</div>" );
-
-  _e( "</div>" );
-}
 
 
 add_filter( 'orbit-filter-field', function( $custom_function, $atts ){
@@ -82,7 +26,8 @@ add_filter( 'orbit-filter-field', function( $custom_function, $atts ){
   if( $atts['type'] == 'tax' && in_array( $atts['typeval'], array( 'services', 'locations' ) ) && $atts['form'] == 'dropdown' ){
 
     $custom_function = function( $atts ){
-      nested_dropdown( $atts );
+      $orbit_form_field = ORBIT_FORM_FIELD::getInstance();
+      $orbit_form_field->nested_dropdown( $atts );
     };
 
   }
