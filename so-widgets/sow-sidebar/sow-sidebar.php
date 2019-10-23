8ee07e11da804444ebfdbd01a4fcfc7f39407436
Steve <anil.stephen66@gmail.com>
2019-10-23 16:03:55 +0530

    Updated sow-sidebar widget

--- so-widgets/sow-sidebar/sow-sidebar.php
+++ so-widgets/sow-sidebar/sow-sidebar.php
@@ -8,6 +8,9 @@ Widget URI:
 Video URI:
 */
 class SOW_SIDEBAR extends SiteOrigin_Widget{
+
+  public $sidebar_details = array();
+
   function __construct(){
 
     $form_options = array(
@@ -15,11 +18,7 @@ class SOW_SIDEBAR extends SiteOrigin_Widget{
         'type' => 'select',
         'label' => __( 'Choose a Sidebar', 'siteorigin-widgets' ),
         'default' => 'single-post-sidebar',
-        'options' => array(
-          'single-post-sidebar' => __( 'Sidebar for Single Post', 'siteorigin-widgets' ),
-          'single-page-sidebar' => __( 'Sidebar for Single Page', 'siteorigin-widgets' ),
-          'homepage-sidebar'    => __( 'Sidebar for Homepage', 'siteorigin-widgets' ),
-        )
+        'options' => $this->get_sidebar_list()
       ),
     );
 
@@ -44,6 +43,24 @@ class SOW_SIDEBAR extends SiteOrigin_Widget{
   }
   function get_style_name($instance){
     return '';
+  }
+
+
+  function get_sidebar_list(){
+
+    $sidebar_list = array();
+    // global $wp_registered_sidebars;
+
+    foreach ( get_option( 'sidebars_widgets' ) as $key => $value) {
+      // $sidebar_list[$key] = $wp_registered_sidebars[$key]['name'];
+      $sidebar_list[$key] = $key;
     }
+    unset( $sidebar_list['wp_inactive_widgets'], $sidebar_list['array_version']  );
+
+    // print_r($sidebar_list);
+
+    return $sidebar_list;
+  }
+
 }
 siteorigin_widget_register( 'sow-sidebar', __FILE__, 'SOW_SIDEBAR' );
