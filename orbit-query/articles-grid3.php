ac6704c5953d94db9c830ea5725aab3636e5ba63
unknown <samvthom16@gmail.com>
2020-01-10 10:42:08 +0530

    Bugfixes for pagination

--- orbit-query/articles-grid3.php
+++ orbit-query/articles-grid3.php
@@ -1,5 +1,5 @@
-<ul class="article-list three-list list-unstyled">
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="article-list three-list list-unstyled">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
-	<li><?php get_template_part( 'partials/content', 'article' );?></li>
+	<li class="orbit-article"><?php get_template_part( 'partials/content', 'article' );?></li>
   <?php endwhile;?>
 </ul>
