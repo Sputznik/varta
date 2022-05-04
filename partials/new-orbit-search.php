4fdb3b2a43520819d32740f915b32a37ebc9c131
psychome004 <anil.stephen66@gmail.com>
2022-05-04 12:05:22 +0530

    #12 Translations for orbit search form

--- partials/new-orbit-search.php
+++ partials/new-orbit-search.php
@@ -2,7 +2,7 @@
 	<div class="orbit-search-form-box">
 		<div class="orbit-search-form-title">
 			<span class='fa-icon'><i class="fa fa-search"></i></span>
-			<span>Search Form</span>
+			<span><?php _e( 'Search Form' ); ?></span>
 			<span class="arrow-down"></span>
 		</div>
 		<?php $this->filters_form( $form );?>
